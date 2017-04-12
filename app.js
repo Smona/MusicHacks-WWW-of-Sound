afaf4e7df7f408f26fc98b66fbc7f9dfaa2ab1f8
Mason Bourgeois <mason.bourgeois@gmail.com>
2017-04-12 12:07:42 -0500

    remove some more unneeded parts for this step

--- app.js
+++ app.js
@@ -45,12 +45,6 @@ Vue.component('audio-deck', {
                 '<i class="material-icons">{{ (playing !== false) ? "stop" : "play_arrow" }}</i>' + 
               '</button>' +
             '</div>',
-  watch: {
-    // Binds the gain node's value to our Vue prop
-    gain: function (newGain) {
-      this.gainNode.gain.value = newGain;
-    }
-  },
   computed: {
     labelArt: function () {
       var artworkUrl = 'none';
